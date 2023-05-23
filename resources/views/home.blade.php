5a3eee3910dd9da5086447e9de2901abb2fe0d81
Xrayya <nur.alkhiyar@gmail.com>
2023-05-23 14:07:26 +0700

    fix cdn error and update home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,18 +6,23 @@
 
 @section('container')
     <main class="col-lg-5 col-md-8 m-auto">
-        <form action="/logout" method="post">
-            @csrf
-            <button type="submit">Logout</button>
-        </form>
         @foreach ($posts as $post)
-            <div class="card text-start">
+            <div class="card text-start my-1">
                 <div class="card-header">
-                    {{ $post->user->display_name }} - {{ $post->user->username }} - {{ $post->created_at->diffForHumans() }}
+                    {{ $post->user->display_name }}
+                    <small class="text-muted">
+                        - <a href="#" class="text-decoration-none text-muted">{{ '@' . $post->user->username }}</a> ·
+                        {{ $post->created_at->diffForHumans() }}
+                    </small>
                 </div>
                 <div class="card-body">
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    //buat like, comment, subrek
+                    <p class="card-text">{{ $post->post }}</p>
+                    <div class="w-100">
+                        <button class="border border-0 bg-transparent text-body-tertiary">
+                            <i class="bi bi-suit-heart"></i>
+                            <i class="bi bi-chat-dots"></i>
+                        </button>
+                    </div>
                 </div>
             </div>
         @endforeach
