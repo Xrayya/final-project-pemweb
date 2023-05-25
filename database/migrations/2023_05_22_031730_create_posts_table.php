5f56a2b9810814713ebeba3621b6279b15964ade
Xrayya <nur.alkhiyar@gmail.com>
2023-05-25 14:36:57 +0700

    change post column data type to string

--- database/migrations/2023_05_22_031730_create_posts_table.php
+++ database/migrations/2023_05_22_031730_create_posts_table.php
@@ -13,7 +13,7 @@ return new class() extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->text('post');
+            $table->string('post');
             $table->bigInteger('parent')->nullable();
             $table->bigInteger('id_user');
             $table->timestamps();
